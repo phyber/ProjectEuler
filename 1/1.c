b8f21921e605b7904a0a9a56ab27885aa7a940fc
phyber <phyber@localhost>
2010-08-04 17:49:13 +0100

    First solution.

---
+++ 1/1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, const char *argv[])
+{
+	int i;
+	unsigned int total = 0;
+	for (i = 3; i < 1000; i++) {
+		if ((i % 3) == 0 && (i % 5) == 0) {
+			total += i;
+			continue;
+		}
+		else if ((i % 3) == 0) {
+			total += i;
+			continue;
+		}
+		else if ((i % 5) == 0) {
+			total += i;
+			continue;
+		}
+	}
+
+	printf("Total of all multiples of 3 or 5 below 1000 == %u\n", total);
+	return 0;
+}
