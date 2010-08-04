0d8e3633afb93ded1935136efc4f56657c92b752
phyber <phyber@localhost>
2010-08-04 20:01:30 +0100

    Even shorter.

--- 1/1.c
+++ 1/1.c
@@ -6,11 +6,7 @@ int main(int argc, const char *argv[])
 	int i;
 	unsigned int total = 0;
 	for (i = 3; i < 1000; i++) {
-		if ((i % 3) == 0 && (i % 5) == 0) {
-			total += i;
-			continue;
-		}
-		else if ((i % 3) == 0 || (i % 5) == 0) {
+		if ( ((i % 3) == 0 && (i % 5) == 0) || ((i % 3) == 0 || (i % 5) == 0) ) {
 			total += i;
 			continue;
 		}
