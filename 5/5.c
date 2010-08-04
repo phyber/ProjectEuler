81f45351b9f7306f75d433b8e13a6886ecf05adb
phyber <phyber@localhost>
2010-08-04 22:02:58 +0100

    Added solution for problem 5.

---
+++ 5/5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+#define TRUE 1
+#define FALSE 0
+
+int is_divisable(unsigned int num) {
+	int i;
+	for (i = 1; i <= 20; i++) {
+		if (num % i != 0) {
+			return FALSE;
+		}
+	}
+
+	return TRUE;
+}
+
+int main(int argc, const char *argv[])
+{
+	unsigned int num = 1;
+	while (1) {
+		if (!is_divisable(num)) {
+			num++;
+		}
+		else {
+			break;
+		}
+	}
+
+	printf("Smallest divisible by 1->20 = %u\n", num);
+
+	return 0;
+}
