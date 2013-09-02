c1ccba8a28ead8628db193b545758dafde334084
phyber <phyber@localhost>
2013-09-02 10:36:41 +0100

    Go solution for #1

---
+++ 001/go.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	total := 0
+	for num := 0; num < 1000; num++ {
+		if num % 3 == 0 || num % 5 == 0 {
+			total += num
+			continue
+		}
+	}
+	fmt.Printf("Total: %d\n", total)
+}
